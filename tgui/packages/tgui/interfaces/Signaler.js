efbe4a56612c1bb3c497222b084736feeb14e12e
Rukofamicom <riskbreakerfamicom2@gmail.com>
2023-03-23 07:07:13 -0500

    Max of 20 signalers

--- tgui/packages/tgui/interfaces/Signaler.js
+++ tgui/packages/tgui/interfaces/Signaler.js
@@ -20,8 +20,6 @@ export const SignalerContent = (props, context) => {
   const {
     code,
     frequency,
-    minFrequency,
-    maxFrequency,
     connection,
   } = data;
   return (
@@ -36,8 +34,6 @@ export const SignalerContent = (props, context) => {
             unit="kHz"
             step={0.2}
             stepPixelSize={6}
-            minValue={minFrequency / 10}
-            maxValue={maxFrequency / 10}
             value={frequency / 10}
             format={value => toFixed(value, 1)}
             width="80px"
@@ -65,7 +61,7 @@ export const SignalerContent = (props, context) => {
             step={1}
             stepPixelSize={6}
             minValue={1}
-            maxValue={100}
+            maxValue={20}
             value={code}
             width="80px"
             onDrag={(e, value) => act('code', {
